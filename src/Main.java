50d6a4a222ceba559a679a28514a6846dceb8e62
BIGJAY5000 <ocampo.616058@swl.k12.oh.us>
2023-11-09 09:28:54 -0500

    hnnr

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,20 @@
+import java.util.Scanner;
+import java.lang.Math;
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner scan = new Scanner(System.in);
+
+        //variables
+        double num;
+        double sqrt;
+
+        System.out.println("Please enter a number");
+        num = scan.nextDouble();
+        sqrt = Math.sqrt(num);
+        if ((sqrt * sqrt) == num) {
+            System.out.println(num + " is a perfect square");
+        } else {
+            System.out.println(num + " is not a perfect square");
+        }
     }
 }
